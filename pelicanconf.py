36a054914f3093c55d77aec464ef4637231c5b35
Jerry <1364812552@qq.com>
2022-01-27 16:18:48 +0800

    modify the publish branch of github pages and change theme of website

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,14 +18,23 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
+        #  ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github','https://github.com/Jerry-se'),
+          ('Email', 'mailto:1364812552@qq.com'),)
+          # ('You can add links in your config file', '#'),
+          # ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Specify name of a built-in theme
+THEME = "notmyidea"
+# Specify name of a theme installed via the pelican-themes tool
+# THEME = "chunk"
+# Specify a customized theme, via path relative to the settings file
+THEME = "pelican-themes/bootstrap"
